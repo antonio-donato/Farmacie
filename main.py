150642fbc644766da38b7618bf3e5562701c145b
donant <donant@gmail.com>
2021-04-07 18:54:47 +0200

    ottimizzazione informazioni

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ for campo in campi:
 print("="*40)
 elementi = []
 index = 0
-for farm in farmacia.readlines()[1:100]:
+quella_precedente = ""
+for farm in farmacia.readlines()[:1000]:
     elementi.append(farm.split(";"))
-    print(f"Elemento numero {elementi[index][0]} \nDenominazione: {elementi[index][3]}\nComune: {elementi[index][7]} ({elementi[index][10]})\n" +
-          f"Coordinates: https://www.google.com/maps/search/?api=1&query={str(elementi[index][18]).replace(',', '.')},{str(elementi[index][19]).replace(',','.')}")
+    if elementi[index][2] != quella_precedente:
+        quella_precedente = elementi[index][2]
+        print(f"Elemento numero {elementi[index][0]}\n"
+              f"Denominazione: {elementi[index][3]}\n"
+              f"Via: {elementi[index][2]}\n"
+              f"Comune: {elementi[index][7]} ({elementi[index][10]})\n" +
+              f"Coordinate: https://www.google.com/maps/search/?api=1&query={str(elementi[index][18]).replace(',', '.')},{str(elementi[index][19]).replace(',','.')}")
     index += 1
 
 farmacia.close()
