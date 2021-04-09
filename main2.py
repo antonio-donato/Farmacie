49ac0c57bee2bef24e463f406736257d02c88840
Antonio Donato <antonio.donato@cgm.com>
2021-04-09 14:00:04 +0200

    test con allineamento stringhe

--- main2.py
+++ main2.py
@@ -15,6 +15,13 @@ index = 0
 quella_precedente = ""
 for farm in farmacia.readlines():
     provvisoria = farm.split(";")
+
+    #  Correggo alcuni errori nel file .csv
+    if provvisoria[16].upper() == "ORDINARA":
+        provvisoria[16] = "Ordinaria"
+    if provvisoria[16].upper() in {"-", "01/04/2021", "LOMBARDIA"}:
+        provvisoria[16] = "*****"
+
     if provvisoria[2].upper() != quella_precedente:
         farmacie_effettive.append(provvisoria)
         quella_precedente = provvisoria[2].upper()
@@ -28,8 +35,16 @@ for farm in farmacia.readlines():
         else:
             tipologia[f"{provvisoria[16].upper()}"] += 1
 
-print(f"Farmacie Effettive: {len(farmacie_effettive)}\n")
-[(print(x + " - " + str(tipologia[x]))) for x in tipologia.keys()]
+print(f"Farmacie Effettive: {len(farmacie_effettive)}\n" + "="*40 + "\n")
 
+#  Titolo centrato
+print("{0:^40}".format("Tabella Tipologie"))
+print("-"*40)
+
+#  allineamento della stringa chiave a destra
+# [(print("{0:>30}".format(x) + " - " + str(tipologia[x]))) for x in tipologia.keys()]
+
+#  Centratura di Tipologia e conteggio
+[(print("{0:^40}".format(f"{x} - {tipologia[x]}"))) for x in tipologia.keys()]
 
 farmacia.close()
